868adb9baf4a4705caa7b78be27629f2b12ff049
yu5 <fukanoyugo+dev@gmail.com>
2024-01-11 02:20:15 +0900

    storybookを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import NextAuthProvider from '@providers/NextAuth';
+import NextAuthProvider from '@/components/providers/NextAuth';
 import { KumaRegistry } from '@kuma-ui/next-plugin/registry';
 import './globals.css';
 
