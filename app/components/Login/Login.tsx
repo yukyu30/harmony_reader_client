a9fd7b5018562aeac0a842513c9d3c779e756eb9
yu5 <fukanoyugo+dev@gmail.com>
2024-01-11 02:40:53 +0900

    Refactor imports and add NextAuth provider

--- app/components/Login/Login.tsx
+++ app/components/Login/Login.tsx
@@ -1,5 +1,5 @@
 import { useSession, signIn, signOut } from 'next-auth/react';
-import { Text, HStack, Image, Button } from '@kuma-ui/core';
+import { HStack, Image, Button } from '@kuma-ui/core';
 
 export default function Component() {
   const { data: session } = useSession();
