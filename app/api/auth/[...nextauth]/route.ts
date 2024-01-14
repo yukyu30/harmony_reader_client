82f706cf667b077f18b114f7b78573aa75ce169b
yu5 <fukanoyugo+dev@gmail.com>
2024-01-15 01:19:01 +0900

    jwtを発行するようにした

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,9 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { Session, User } from 'next-auth';
+import * as jose from 'jose';
+import { JWT } from 'next-auth/jwt';
+import { Tilt_Prism } from 'next/font/google';
 
 export const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
@@ -9,7 +13,41 @@ export const authOptions = {
       clientSecret: String(process.env.GOOGLE_CLIENT_SECRET),
     }),
   ],
+  callbacks: {
+    session: async ({
+      session,
+      token,
+    }: {
+      session: Session;
+      user: User;
+      token: JWT;
+    }) => {
+      if (token.sub != null) {
+        const payload = {
+          sub: token.sub,
+          email: token.email,
+        };
+
+        const secret = new TextEncoder().encode(String(process.env.APP_SECRET));
+
+        const alg = 'HS256';
+
+        const appAccessToken = await new jose.SignJWT(payload)
+          .setProtectedHeader({ alg })
+          .setExpirationTime('30d')
+          .setJti(String(token.jti))
+          .sign(secret);
+        return Promise.resolve({
+          ...session,
+          appAccessToken: appAccessToken,
+        });
+      }
+
+      return session;
+    },
+  },
 };
+
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
